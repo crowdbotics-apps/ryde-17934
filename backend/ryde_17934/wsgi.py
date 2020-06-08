16af01fa75a67b83603a958cf53ba81c23258ee2
Crowdbotics <team@crowdbotics.com>
2020-06-08 13:55:12 +0000

    Update models

--- backend/ryde_17934/wsgi.py
+++ backend/ryde_17934/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ryde_17934.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ryde_17934.settings")
 
 application = get_wsgi_application()
